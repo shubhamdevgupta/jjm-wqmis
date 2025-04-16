3676434fe1808c6b443122d1e6c2c9805a416492
Rishabh vaid <rishabhvaid69@gmail.com>
2025-04-16 15:10:05 +0530

    add design

---
+++ lib/views/DWSM/Schoolpage.dart
@@ -0,0 +1,264 @@
+import 'package:flutter/material.dart';
+import 'package:jjm_wqmis/providers/ParameterProvider.dart';
+import 'package:jjm_wqmis/utils/AppConstants.dart';
+import 'package:provider/provider.dart';
+
+import '../../models/LabInchargeResponse/AllLabResponse.dart';
+import '../../providers/masterProvider.dart';
+import '../../services/LocalStorageService.dart';
+import '../../utils/CustomSearchableDropdown.dart';
+import '../../utils/LoaderUtils.dart';
+
+class Schoolpage extends StatefulWidget {
+  @override
+  _Schoolpage createState() => _Schoolpage();
+}
+
+class _Schoolpage extends State<Schoolpage> {
+  late Masterprovider masterProvider;
+  final LocalStorageService _localStorage = LocalStorageService();
+
+  @override
+  void initState() {
+    super.initState();
+    masterProvider = Provider.of<Masterprovider>(context, listen: false);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<ParameterProvider>(context, listen: false).isLab=true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: Provider.of<ParameterProvider>(context, listen: false),
+      child: Consumer<ParameterProvider>(
+        builder: (context, provider, child) {
+          return Container(
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+              body: Stack(
+                children: [
+                  SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          CustomSearchableDropdown(
+                            title: "",
+                            value: provider.selectedLab,
+                            items: provider.labList
+                                .map((lab) =>
+                            lab.text ?? '') // Display text, not value
+                                .toList(),
+                            onChanged: (selectedLabText) {
+                              if (selectedLabText == null)
+                                return; // Handle null case
+
+                              final selectedLab = provider.labList.firstWhere(
+                                    (lab) => lab.text == selectedLabText,
+                                orElse: () => Alllabresponse(
+                                    value: null,
+                                    text:
+                                    null), // Default to a nullable object
+                              );
+                              provider.cart!.clear();
+                              provider.setSelectedLab(selectedLab.value);
+                              if (provider.isLabSelected) {
+                                provider.fetchAllParameter(
+                                  selectedLab.value!,
+                                  masterProvider.selectedStateId ?? "0",
+                                  "0",
+                                  _localStorage.getString(AppConstants.prefRegId).toString(),
+                                  "0",
+                                );
+                              }
+                            },
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Visibility(
+                            visible: provider.isLabSelected,
+                            child: Card(
+                              elevation: 5,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              margin: EdgeInsets.all(5),
+                              color: Colors.white,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      'Select Parameter Type:',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    DropdownButton<int>(
+                                      isExpanded: true,
+                                      value: provider.parameterType ?? 1,
+                                      items: const [
+                                        DropdownMenuItem(
+                                            value: 1,
+                                            child: Text('All Parameter')),
+                                        DropdownMenuItem(
+                                            value: 2,
+                                            child: Text('Chemical Parameter')),
+                                        DropdownMenuItem(
+                                            value: 3,
+                                            child: Text(
+                                                'Bacteriological Parameter')),
+                                      ],
+                                      onChanged: (value) {
+                                        if (value == null) return;
+                                        provider.setParameterType(value);
+                                        provider.cart!.clear();
+                                        provider.fetchAllParameter(
+                                          provider.selectedLab!,
+                                          masterProvider.selectedStateId!,
+                                          "0",
+                                          _localStorage.getString(AppConstants.prefRegId).toString(),
+                                          value.toString(),
+                                        );
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Visibility(
+                            visible: provider.isLabSelected,
+                            child: Card(
+                              elevation: 5,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              margin: EdgeInsets.all(5),
+                              color: Colors.white,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SingleChildScrollView(
+                                      scrollDirection: Axis.horizontal,
+                                      child: DataTable(
+                                        columnSpacing: 20,
+                                        headingRowHeight: 50,
+                                        dataRowHeight: 60,
+                                        columns: const <DataColumn>[
+                                          DataColumn(
+                                            label: Text(
+                                              'Select Test',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 16,
+                                                color: Colors.blueGrey,
+                                              ),
+                                            ),
+                                          ),
+                                          DataColumn(
+                                            label: Text(
+                                              'Test Price',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 16,
+                                                color: Colors.blueGrey,
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                        rows: provider.parameterList.map((param) {
+                                          bool isSelected = provider.cart!.any(
+                                                (item) => item.parameterId == param.parameterId,
+                                          );
+
+                                          return DataRow(
+                                            cells: <DataCell>[
+                                              DataCell(
+                                                GestureDetector(
+                                                  onTap: () {
+                                                    provider.toggleCart(param);
+                                                  },
+                                                  child: Row(
+                                                    children: [
+                                                      Checkbox(
+                                                        value: isSelected,
+                                                        onChanged: (bool? value) {
+                                                          print('the selected value labview------- $value');
+                                                          if (value != null) {
+                                                            provider.toggleCart(param);
+                                                          }
+                                                        },
+                                                      ),
+                                                      SizedBox(width: 10),
+                                                      SizedBox(
+                                                        width: 150,
+                                                        child: Text(
+                                                          param.parameterName,
+                                                          overflow: TextOverflow.ellipsis,
+                                                          style: TextStyle(fontSize: 14),
+                                                        ),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ),
+                                              ),
+                                              DataCell(
+                                                GestureDetector(
+                                                  onTap: () {
+                                                    provider.toggleCart(param);
+                                                  },
+                                                  child: Text(
+                                                    param.deptRate.toString(),
+                                                    style: TextStyle(fontSize: 14),
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          );
+                                        }).toList(),
+                                      ),
+                                    ),
+
+
+                                    const SizedBox(height: 20),
+                                    Text(
+                                      'Cart: ${provider.cart!.length}',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+
+                  if (provider.isLoading)
+                    LoaderUtils.conditionalLoader(isLoading: provider.isLoading)
+                ],
+              ),
+
+
+
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+
+}
---
+++ lib/views/DWSM/submit_info.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:jjm_wqmis/providers/ParameterProvider.dart';
+import 'package:jjm_wqmis/providers/masterProvider.dart';
+import 'package:jjm_wqmis/utils/LoaderUtils.dart';
+import 'package:provider/provider.dart';
+
+import '../../services/LocalStorageService.dart';
+
+import '../../utils/AppConstants.dart';
+import 'AnganwadiPage.dart';
+import 'Schoolpage.dart';
+
+class SubmitInfo extends StatefulWidget {
+
+  @override
+  _SubmitInfo createState() => _SubmitInfo();
+}
+
+class _SubmitInfo extends State<SubmitInfo>
+    with SingleTickerProviderStateMixin {
+  late TabController mTabController;
+  late ParameterProvider paramProvider;
+  late Masterprovider masterProvider;
+  final LocalStorageService _localStorage = LocalStorageService();
+
+  @override
+  void initState() {
+    super.initState();
+    mTabController = TabController(length: 2, vsync: this, initialIndex: 0);
+
+    // Get providers
+    paramProvider = Provider.of<ParameterProvider>(context, listen: false);
+    masterProvider = Provider.of<Masterprovider>(context, listen: false);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<ParameterProvider>(context, listen: false).clearData();
+    });
+    mTabController.addListener(() {
+      if (mTabController.indexIsChanging) return; // Prevent duplicate calls
+
+
+    });
+
+
+  }
+
+
+
+
+
+  @override
+  void dispose() {
+    mTabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Tab> myTabs = <Tab>[
+      const Tab(
+        icon: Icon(Icons.school_sharp, color: Colors.white),
+        child: Text(
+          "School",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      const Tab(
+        icon: Icon(
+          Icons.house_sharp,
+          color: Colors.white,
+        ),
+        child: Text(
+          'Anganwadi',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    ];
+
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(image: AssetImage('assets/header_bg.png')),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: const Text("Select School/ Angawadi", style: TextStyle(color: Colors.white)),
+          automaticallyImplyLeading: false,
+          elevation: 5,
+          centerTitle: true,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.horizontal(
+              left: Radius.circular(8),
+              right: Radius.circular(8),
+            ),
+          ),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () {
+              if (Navigator.of(context).canPop()) {
+                Navigator.pop(context);
+              } else {
+                Navigator.pushReplacementNamed(
+                    context, AppConstants.navigateToSaveSample);
+              }
+            },
+          ),
+          bottom: TabBar(
+            controller: mTabController,
+            tabs: myTabs,
+            labelColor: Colors.white, // White for selected tab text
+            unselectedLabelColor: Colors.white70, // Slightly faded for unselected tabs
+            labelStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            unselectedLabelStyle: const TextStyle(fontSize: 14),
+            indicator: BoxDecoration(
+              color: Color(0xFF5FAFE5), // Light blue indicator
+              borderRadius: BorderRadius.circular(8),
+            ),
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicatorPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+          ),
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              color: Colors.blueAccent,
+              gradient: const LinearGradient(
+                colors: [
+                  Color(0xFF096DA8), // Dark blue
+                  Color(0xFF3C8DBC),  // jjm blue color
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,// End at the bottom center
+              ),
+            ),
+          ),
+        ),
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            return SizedBox(
+              height: constraints.maxHeight - 45,
+              child: TabBarView(
+                controller: mTabController,
+                children: [
+                  Schoolpage(),
+                  Anganwadipage(),
+                  LoaderUtils.conditionalLoader(isLoading: paramProvider.isLoading)
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
