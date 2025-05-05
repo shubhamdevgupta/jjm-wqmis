549a7063e6a6e3e3ae185efd7cb918190b1762dc
Shubham Gupta <75482143+shubhamdevgupta@users.noreply.github.com>
2025-05-05 15:39:49 +0530

    add dynamic data

--- lib/views/DWSM/DemonstrationScreen.dart
+++ lib/views/DWSM/DemonstrationScreen.dart
@@ -1,24 +1,30 @@
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:jjm_wqmis/utils/LoaderUtils.dart';
 import 'package:provider/provider.dart';
 
-import '../../models/DWSM/DwsmDashboard.dart';
 import '../../providers/dwsmProvider.dart';
-
+import '../../services/LocalStorageService.dart';
+import '../../utils/AppConstants.dart';
 
 class Demonstrationscreen extends StatefulWidget {
-
   @override
   State<Demonstrationscreen> createState() => _DemonstrationscreenState();
 }
 
 class _DemonstrationscreenState extends State<Demonstrationscreen> {
-
+  final LocalStorageService _localStorage = LocalStorageService();
+  String stateId = '';
+  String districtId = '';
   @override
   void initState() {
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      Provider.of<DwsmDashboardProvider>(context, listen: false).loadDwsmDashboardData(int.parse("31"), 471, "2025-2026");
+      stateId = _localStorage.getString(AppConstants.prefStateId)!;
+      districtId = _localStorage.getString(AppConstants.prefDistrictId)!;
+
+      Provider.of<DwsmDashboardProvider>(context, listen: false)
+          .loadDwsmDashboardData(int.parse(stateId), int.parse(districtId), "2025-2026");
     });
     super.initState();
   }
@@ -30,15 +36,14 @@ class _DemonstrationscreenState extends State<Demonstrationscreen> {
           automaticallyImplyLeading: false,
           // Removes the default back button
           centerTitle: true,
-          title:  const Text(
-           "Demonstrations List",
+          title: const Text(
+            "Demonstrations List",
             style: TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
               color: Colors.white,
             ),
           ),
-
 
           leading: IconButton(
             icon: const Icon(Icons.arrow_back, color: Colors.white),
@@ -64,164 +69,175 @@ class _DemonstrationscreenState extends State<Demonstrationscreen> {
           ),
           elevation: 5,
         ),
+        body: Consumer<DwsmDashboardProvider>(
+            builder: (context, provider, child) {
+          return provider.isLoading
+              ? LoaderUtils.conditionalLoader(isLoading: provider.isLoading)
+              : ListView.builder(
+                  itemCount: provider.villages.length,
+                  itemBuilder: (context, index) {
+                    final village = provider.villages[index];
 
-      body: Consumer<DwsmDashboardProvider>(builder : (context,provider , child){
-        return
-          provider.isLoading?LoaderUtils.conditionalLoader(isLoading: provider.isLoading):
-          ListView.builder(
-          itemCount: provider.villages.length,
-          itemBuilder: (context, index) {
-            final village = provider.villages[index];
+                    final base64String = village.photo.split(',').last;
+                    final imageBytes = base64Decode(base64String);
 
-            final base64String = village.photo.split(',').last;
-            final imageBytes = base64Decode(base64String);
-
-            return Container(
-              margin: const EdgeInsets.all(12),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                border: Border.all(color: Colors.blueAccent.shade100, width: 1.5),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.blueAccent.withOpacity(0.1),
-                    blurRadius: 8,
-                    offset: const Offset(0, 4),
-                  ),
-                ],
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // Image with overlay label
-                  Stack(
-                    children: [
-                      ClipRRect(
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(16),
-                          topRight: Radius.circular(16),
-                        ),
-                        child: Image.memory(
-                          imageBytes,
-                          width: double.infinity,
-                          height: 180,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Positioned(
-                        bottom: 10,
-                        left: 10,
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                          decoration: BoxDecoration(
-                            color: Colors.black.withOpacity(0.6),
-                            borderRadius: BorderRadius.circular(6),
+                    return Container(
+                      margin: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        border: Border.all(
+                            color: Colors.blueAccent.shade100, width: 1.5),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.blueAccent.withOpacity(0.1),
+                            blurRadius: 8,
+                            offset: const Offset(0, 4),
                           ),
-                          child: const Text(
-                            "School Photo",
-                            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
-                          ),
-                        ),
+                        ],
                       ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // Title Row
-                        Row(
-                          children: const [
-                            Icon(Icons.location_on_outlined, color: Colors.blue, size: 20),
-                            SizedBox(width: 8),
-                            Text(
-                              "School Details",
-                              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 12),
-
-                        // School Name with icon
-                        Row(
-                          children: [
-                            const Icon(Icons.school, color: Colors.deepPurple, size: 20),
-                            const SizedBox(width: 8),
-                            Expanded(
-                              child: Text(
-                                "schoolName", // assume it's a string variable
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 15,
-                                  color: Colors.deepPurple,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // Image with overlay label
+                          Stack(
+                            children: [
+                              ClipRRect(
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(16),
+                                  topRight: Radius.circular(16),
+                                ),
+                                child: Image.memory(
+                                  imageBytes,
+                                  width: double.infinity,
+                                  height: 180,
+                                  fit: BoxFit.cover,
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 10),
-
-                        // Details Box
-                        Container(
-                          padding: const EdgeInsets.all(12),
-                          decoration: BoxDecoration(
-                            color: Colors.blue.shade50,
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              _buildInfoRow("State", village.stateName),
-                              _buildInfoRow("District", village.districtName),
-                              _buildInfoRow("Block", village.blockName),
-                              _buildInfoRow("Panchayat", village.panchayatName),
-                              _buildInfoRow("Village", village.villageName),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(height: 12),
-
-                        // User Remark
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Icon(Icons.comment, color: Colors.teal, size: 20),
-                            const SizedBox(width: 8),
-                            Expanded(
-                              child: Container(
-                                padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                                decoration: BoxDecoration(
-                                  color: Colors.teal[50],
-                                  borderRadius: BorderRadius.circular(12),
-                                ),
-                                child: Text(
-                                   "No remark provided",
-                                  style: TextStyle(
-                                    color: Colors.teal[900],
-                                    fontWeight: FontWeight.w500,
+                              Positioned(
+                                bottom: 10,
+                                left: 10,
+                                child: Container(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10, vertical: 5),
+                                  decoration: BoxDecoration(
+                                    color: Colors.black.withOpacity(0.6),
+                                    borderRadius: BorderRadius.circular(6),
+                                  ),
+                                  child: const Text(
+                                    "School Photo",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w600),
                                   ),
                                 ),
                               ),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                // Title Row
+                                Row(
+                                  children: const [
+                                    Icon(Icons.location_on_outlined,
+                                        color: Colors.blue, size: 20),
+                                    SizedBox(width: 8),
+                                    Text(
+                                      "School Details",
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 12),
+
+                                // School Name with icon
+                                Row(
+                                  children: [
+                                    const Icon(Icons.school,
+                                        color: Colors.deepPurple, size: 20),
+                                    const SizedBox(width: 8),
+                                    Expanded(
+                                      child: Text(
+                                        "schoolName",
+                                        // assume it's a string variable
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: 15,
+                                          color: Colors.deepPurple,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 10),
+
+                                // Details Box
+                                Container(
+                                  padding: const EdgeInsets.all(12),
+                                  decoration: BoxDecoration(
+                                    color: Colors.blue.shade50,
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      _buildInfoRow("State", village.stateName),
+                                      _buildInfoRow(
+                                          "District", village.districtName),
+                                      _buildInfoRow("Block", village.blockName),
+                                      _buildInfoRow(
+                                          "Panchayat", village.panchayatName),
+                                      _buildInfoRow(
+                                          "Village", village.villageName),
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(height: 12),
+
+                                // User Remark
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Icon(Icons.comment,
+                                        color: Colors.teal, size: 20),
+                                    const SizedBox(width: 8),
+                                    Expanded(
+                                      child: Container(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 12, vertical: 8),
+                                        decoration: BoxDecoration(
+                                          color: Colors.teal[50],
+                                          borderRadius:
+                                              BorderRadius.circular(12),
+                                        ),
+                                        child: Text(
+                                          "No remark provided",
+                                          style: TextStyle(
+                                            color: Colors.teal[900],
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            );
-
-
-
-          },
-        );
-      }
-      )
-
-    );
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+        }));
   }
+
   Widget _buildInfoRow(String label, String value) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 4.0),
@@ -238,6 +254,4 @@ class _DemonstrationscreenState extends State<Demonstrationscreen> {
       ),
     );
   }
-
-
 }
