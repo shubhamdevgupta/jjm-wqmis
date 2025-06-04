5a51768dc1759ca36e6d5b627fb15b755895a725
Shubham Gupta <75482143+shubhamdevgupta@users.noreply.github.com>
2025-06-04 15:55:06 +0530

    add firebase crash analyitics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:jjm_wqmis/providers/ErrorProvider.dart';
 import 'package:jjm_wqmis/providers/ParameterProvider.dart';
@@ -18,6 +20,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await LocalStorageService.init();
   await UserSessionManager().init();
+  await Firebase.initializeApp();
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
 
   runApp(
     MultiProvider(
